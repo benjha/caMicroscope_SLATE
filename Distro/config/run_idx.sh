e83601cac80716c5c586a0f86b0efe6cb8435ae5
Hernandez Arreguin <e8h@mac02784.ornl.gov>
2021-02-16 14:14:46 -0500

    Adding Distro

---
+++ Distro/config/run_idx.sh
@@ -0,0 +1,14 @@
+until mongo --host $CA_MONGO_HOST --eval "print(\"Connected!\")" > /dev/null
+do
+    sleep 2
+done
+# this won't do anything if collections already exist
+mongo --quiet --host $CA_MONGO_HOST camic config/mongo_collections.js
+mongo --quiet --host $CA_MONGO_HOST camic config/mongo_idx.js
+echo "indexes created (note that some failures will not show up here)"
+mongo --quiet --host $CA_MONGO_HOST camic config/add_users.js
+echo "users created"
+mongo --quiet --host $CA_MONGO_HOST camic config/default_data.js
+# uncommenting this could be useful for testing
+# mongo --host ca-mongo camic2 /config/test_seed.js
+echo "defaults added"
